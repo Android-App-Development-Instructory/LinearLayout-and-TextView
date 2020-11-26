ca80e40ba9cdb1c1b348868ae3e87dc31669f23a
User <https://github.com/karno786/Sqlite_git_test>
2020-11-27 01:34:06 +0600

    01. LinearLayout and RelativeLayout, TextView, WeightSum - Android Development Course Basic To Advanced

---
+++ app/src/main/java/com/alaminkarno/linearrelativetextviewandweightsum/Assignment01Activity.java
@@ -0,0 +1,14 @@
+package com.alaminkarno.linearrelativetextviewandweightsum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Assignment01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment01);
+    }
+}
